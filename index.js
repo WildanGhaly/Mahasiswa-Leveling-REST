b2d69b5c73ec2c316c2147064a7f47f3e218384e
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-10 18:09:28 +0700

    feat: add login jwt mechanism (dirty)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
 const mysql = require('mysql2');
+const cookieParser = require('cookie-parser');
 
 const mysqlConfig = {
   host: process.env.DB_HOST,
@@ -15,9 +16,14 @@ const mysqlConfig = {
 
 let con = null;
 
-app.use(express.json())
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, // Izinkan pengiriman cookie (sesuai dengan withCredentials pada axios)
+};
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
 
 let refreshTokens = []
 const posts = [
@@ -50,6 +56,7 @@ function authenticateToken(req, res, next) {
 }
 
 function connect() {
+  if (con) return;
   try {
     con =  mysql.createConnection(mysqlConfig);
     con.connect(function(err) {
@@ -73,8 +80,9 @@ app.post('/login', (req, res) => {
   const password = req.body.password
 
   connect();
-
+  console.log(username, password)
   con.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function (err, result, fields) {
+    console.log("Was here", result);
     if (err) throw err;
     console.log(result);
     if (result.length > 0) {
@@ -82,10 +90,14 @@ app.post('/login', (req, res) => {
       const accessToken = generateAccessToken(user)
       const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
       refreshTokens.push(refreshToken)
-      res.json({ accessToken: accessToken, refreshToken: refreshToken, user: user })
+      console.log(refreshTokens);
+      res.cookie('test', 'hello world', { httpOnly: true, secure: true, sameSite: 'none' });
+      res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, sameSite: 'none' });
+      res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'none' });
+      res.json({ user: user })
     } else {
       res.sendStatus(401)
-    }
+    } 
   }); 
 })
 
@@ -101,16 +113,50 @@ app.post('/token', (req, res) => {
   })
 })
 
+app.get('/check-status', (req, res) => {
+  console.log('check-status');
+  const refreshTokens = req.cookies.refreshToken;
+  const accessTokens = req.cookies.accessToken;
+  console.log(refreshTokens);
+  console.log(accessTokens);
+  res.cookie('test01', 'hello world', { httpOnly: true, secure: true, sameSite: 'none' });
+
+  if (!refreshTokens) return res.json({ isLoggedIn: false })
+  if (!accessTokens) {
+    jwt.verify(refreshTokens, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if (err) return res.json({ isLoggedIn: false })
+      const accessToken = generateAccessToken({ name: user.name })
+      res.json({ isLoggedIn: true, accessToken: accessToken })
+    })
+  } else {
+  
+  }
+  
+  res.cookie('test', 'hello world', { httpOnly: true, secure: true, sameSite: 'none' });
+  console.log('check-status---------');
+  res.json({ isLoggedIn: true })
+})
+
 // Endpoint to logout
 app.delete('/logout', (req, res) => {
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-  res.sendStatus(204)
+  const refreshToken = req.cookies.refreshToken;
+  refreshTokens = refreshTokens.filter(token => token !== refreshToken);
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+  res.sendStatus(204);
 })
 
 // Protected route to get posts
 app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name))
 })
+
+app.get('/cookie', (req, res) => {
+  res.cookie('jwt', 'hello world', { httpOnly: true, secure: true, sameSite: 'none' });
+  res.cookie('refreshToken', 'hello world', { httpOnly: true, secure: true, sameSite: 'none' });
+  console.log(req.cookies.jwt);
+  res.json({ message: 'cookie set' });
+});
 
 // Listen on one port
 app.listen(8080, () => {
