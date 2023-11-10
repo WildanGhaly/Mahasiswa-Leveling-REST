adbf1a51dcdfad211950aec384a8f2796254466c
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-10 22:10:32 +0700

    feat: add register mechanism

--- index.js
+++ index.js
@@ -88,6 +88,26 @@ app.post('/login', (req, res) => {
   }); 
 })
 
+app.post('/register', (req, res) => {
+  const username = req.body.username
+  const email = req.body.email
+  const password = req.body.password
+
+  connect();
+  console.log("Registering... ", username, password) 
+  con.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password], function (err, result, fields) {
+    if (err) throw err;
+    const user = { name: username }
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    res.cookie('test', 'hello world', { httpOnly: true, secure: true, sameSite: 'none' });
+    res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, sameSite: 'none' });
+    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'none' });
+    res.json({ user: user })
+  });
+})
+
 // Endpoint to refresh token
 app.post('/token', (req, res) => {
   const refreshToken = req.body.token
