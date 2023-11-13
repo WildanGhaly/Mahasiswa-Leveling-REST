e151734dcb03117f2d7c294b335586ca6e9eadc2
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-13 10:39:36 +0700

    refactor: split authentication from index

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
-const mysql = require('mysql2');
 const cookieParser = require('cookie-parser');
 
 const con = require('./database.js');
+const { generateAccessToken, authenticateToken } = require('./auth.js');
+
+const authController = require('./controllers/authController');
 
 const corsOptions = {
   origin: 'http://localhost:3000',
@@ -20,71 +22,12 @@ app.use(cors(corsOptions));
 
 let refreshTokens = []
 
-// Function to generate access token
-function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10s' })
-}
-
-// Middleware to authenticate token
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
-}
-
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-// Endpoint for login
-app.post('/login', (req, res) => {
-  // Authenticate User
-  const username = req.body.username
-  const password = req.body.password
-
-  console.log("Logging in... ", username, password)
-  con.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function (err, result, fields) {
-    if (err) throw err;
-    if (result.length > 0) {
-      const user = { name: username }
-      const accessToken = generateAccessToken(user)
-      const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-      refreshTokens.push(refreshToken)
-      res.cookie('test', 'hello world', { httpOnly: true, secure: true, sameSite: 'none' });
-      res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, sameSite: 'none' });
-      res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'none' });
-      res.json({ user: user })
-    } else {
-      res.sendStatus(401)
-    } 
-  }); 
-})
-
-app.post('/register', (req, res) => {
-  const username = req.body.username
-  const email = req.body.email
-  const password = req.body.password
-
-  console.log("Registering... ", username, password) 
-  con.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password], function (err, result, fields) {
-    if (err) throw err;
-    const user = { name: username }
-    const accessToken = generateAccessToken(user)
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    refreshTokens.push(refreshToken)
-    res.cookie('test', 'hello world', { httpOnly: true, secure: true, sameSite: 'none' });
-    res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, sameSite: 'none' });
-    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'none' });
-    res.json({ user: user })
-  });
-})
+app.use(authController); 
 
 // Endpoint to refresh token
 app.post('/token', (req, res) => {
