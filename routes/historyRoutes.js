02d62c50868d123c9ec0c25346805bf57b5fef58
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-14 14:19:40 +0700

    feat: add history controller

--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -1,26 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { checkToken } = require("../middleware/authMiddleware");
-const con = require("../database/database.js");
+const historyController = require("../controllers/historyController");
 
-router.get("/", checkToken, (req, res) => {
-    if (!req.isTokenValid) {
-      return res.json({ isLoggedIn: false, username: null });
-    }
-  
-    const query = `
-      SELECT h.history_id as HistoryID, h.quantity as HistoryQuantity,
-             p.productname as HistoryProductName, h.timestamp as HistoryDate,
-             p.imagepath as HistoryImagePath
-      FROM users u
-      JOIN history h ON u.id = h.user_id
-      JOIN products p ON p.productid = h.product_id
-      WHERE username = ?`;
-  
-    con.query(query, [req.username], function (err, result, fields) {
-      if (err) throw err;
-      res.json(result);
-    });
-});
+router.get("/", checkToken, historyController.getHistory);
 
 module.exports = router;
