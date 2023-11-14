b518be0d165e3a6c84fa3e8d36057b2d9c766ece
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-14 14:19:51 +0700

    feat: add auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,73 @@
+// authController.js
+const jwt = require("jsonwebtoken");
+const con = require("../database/database.js");
+const { generateAccessToken } = require("../middleware/tokenMiddleware.js");
+const { addRefreshToken } = require("../middleware/tokenMiddleware.js");
+
+exports.login = (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log("Logging in... ", username, password);
+  con.query(
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    [username, password],
+    function (err, result, fields) {
+      if (err) throw err;
+      if (result.length > 0) {
+        const user = { name: username };
+        const accessToken = generateAccessToken(user);
+        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+        addRefreshToken(refreshToken);
+        res.cookie("accessToken", accessToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        });
+        res.cookie("refreshToken", refreshToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        });
+        res.json({ user: user });
+      } else {
+        res.sendStatus(401);
+      }
+    }
+  );
+};
+
+exports.register = (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log("Registering... ", username, password);
+  con.query(
+    "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+    [username, email, password],
+    function (err, result, fields) {
+      if (err) throw err;
+      const user = { name: username };
+      const accessToken = generateAccessToken(user);
+      const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+      addRefreshToken(refreshToken);
+      res.cookie("test", "hello world", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
+      res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
+      res.json({ user: user });
+    }
+  );
+};
