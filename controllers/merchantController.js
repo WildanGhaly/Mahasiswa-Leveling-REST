c0c1dec3e669a3123156ec3b4baaeca8c97aaa54
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-14 14:20:59 +0700

    feat: add merchant controller

---
+++ controllers/merchantController.js
@@ -0,0 +1,21 @@
+// merchantController.js
+const con = require("../database/database.js");
+
+exports.getMerchants = (req, res) => {
+  if (!req.isTokenValid) {
+    return res.json({ isLoggedIn: false, username: null });
+  }
+
+  const query = `
+    SELECT p.productid AS MerchantID, up.quantity AS MerchantQuantity,
+           p.productname AS MerchantName, p.imagepath AS MerchantImagePath
+    FROM users u
+    JOIN user_product up ON u.id = up.user_id
+    JOIN products p ON up.product_id = p.productid
+    WHERE u.username = ?`;
+
+  con.query(query, [req.username], function (err, result, fields) {
+    if (err) throw err;
+    res.json(result);
+  });
+};
--- routes/merchantRoutes.js
+++ routes/merchantRoutes.js
@@ -1,26 +1,9 @@
 const express = require("express");
 const routes = express.Router();
-const con = require("../database/database.js");
 const { checkToken } = require("../middleware/authMiddleware");
+const merchantController = require("../controllers/merchantController");
 
 // Endpoint '/merchants'
-routes.get("/", checkToken, (req, res) => {
-    if (!req.isTokenValid) {
-      return res.json({ isLoggedIn: false, username: null });
-    }
-  
-    const query = `
-      SELECT p.productid AS MerchantID, up.quantity AS MerchantQuantity,
-             p.productname AS MerchantName, p.imagepath AS MerchantImagePath
-      FROM users u
-      JOIN user_product up ON u.id = up.user_id
-      JOIN products p ON up.product_id = p.productid
-      WHERE u.username = ?`;
-  
-    con.query(query, [req.username], function (err, result, fields) {
-      if (err) throw err;
-      res.json(result);
-    });
-  });
+routes.get("/", checkToken, merchantController.getMerchants);
 
 module.exports = routes;
