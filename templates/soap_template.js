68fdeaf685c6ee1acb484f32d9d62e1d2308cc60
Haidar <16521522@mahasiswa.itb.ac.id>
2023-11-16 06:15:20 +0700

    feat: add soap template xml

---
+++ templates/soap_template.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+
+// const url = process.env.SOAP_URL;
+// "X-API-KEY": process.env.SOAP_API_KEY,
+
+const headers = {
+  "Content-Type": "text/xml;charset=UTF-8",
+  "X-API-KEY": "RestClient",
+};
+
+const topup_soap_template = `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+<Body>
+    <topupPoint xmlns="http://service.example.org/">
+        <arg0 xmlns="">%d</arg0>
+        <arg1 xmlns="">%d</arg1>
+    </topupPoint>
+</Body>
+</Envelope>`;
+
+const topup = {
+  url: "http://localhost:8081/topup",
+  headers: headers,
+  template: topup_soap_template
+};
+
+const buyProduct_soap_template = `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+<Body>
+    <buyProduct xmlns="http://service.example.org/">
+        <arg0 xmlns="">%d</arg0>
+        <arg1 xmlns="">%d</arg1>
+        <arg2 xmlns="">%d</arg2>
+    </buyProduct>
+</Body>
+</Envelope>`;
+
+const buyProduct = {
+  url: "http://localhost:8081/buyProduct",
+  headers: headers,
+  template: buyProduct_soap_template
+};
+
+const getHistory_soap_template = `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+<Body>
+    <getHistory xmlns="http://service.example.org/">
+        <arg0 xmlns="">%d</arg0>
+    </getHistory>
+</Body>
+</Envelope>`;
+
+const getHistory = {
+  url: "http://localhost:8081/getHistory",
+  headers: headers,
+  template: getHistory_soap_template
+};
+
+module.exports = { topup };
+
+
