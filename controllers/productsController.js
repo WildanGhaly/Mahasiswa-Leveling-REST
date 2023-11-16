db5c4808e170d1cd6237a643022b935e65063a4b
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-16 13:18:03 +0700

    feat: add filter and search mechanism

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -24,10 +24,31 @@ exports.getTotalProducts = (req, res) => {
   console.log("Menghitung total produk... " + req.params.search);
 
   const search = req.params.search;
+  const filter = req.params.filter;
   var query = "SELECT COUNT(*) AS TotalProducts FROM products";
 
   if (search) {
     query += " WHERE ProductName LIKE '%" + search + "%'";
+  }
+
+  if (search && filter) {
+    query += " AND ";
+  }
+
+  if (!search && filter) {
+    query += " WHERE ";
+  }
+
+  if (filter) {
+    if (filter == "Available") {
+      query += " stockquantity > 0 ";
+    }
+    else if (filter == "Not Available") {
+      query += " stockquantity = 0 ";
+    }
+    else {
+      query += " stockquantity >= 0 ";
+    } 
   }
 
   con.query(query, function (
@@ -45,12 +66,33 @@ exports.getProductByPage = (req, res) => {
   const page = req.params.page;
   const limit = req.params.limit;
   const search = req.params.search;
+  const filter = req.params.filter;
   const offset = (page - 1) * limit;
   var query = "SELECT * FROM products";
 
 
   if (search) {
     query += " WHERE ProductName LIKE '%" + search + "%'";
+  }
+
+  if (search && filter) {
+    query += " AND ";
+  }
+
+  if (!search && filter) {
+    query += " WHERE ";
+  }
+
+  if (filter) {
+    if (filter == "Available") {
+      query += " stockquantity > 0 ";
+    }
+    else if (filter == "Not Available") {
+      query += " stockquantity = 0 ";
+    }
+    else {
+      query += " stockquantity >= 0 ";
+    }
   }
   
   query += " LIMIT " + limit + " OFFSET " + offset;
