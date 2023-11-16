945c34d4de172363f45648bacbe1f42dff3041b6
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-17 06:13:27 +0700

    feat: add merchants search filter endpoint

--- controllers/merchantController.js
+++ controllers/merchantController.js
@@ -20,3 +20,62 @@ exports.getMerchants = (req, res) => {
     res.json(result);
   });
 };
+
+exports.getTotalMerchants = (req, res) => {
+  if (!req.isTokenValid) {
+    return res.json({ isLoggedIn: false, username: null });
+  }
+
+  const search = req.params.search;
+  var query = "SELECT COUNT(*) AS TotalMerchants FROM users u JOIN user_product up ON u.id = up.user_id JOIN products p ON p.productid = up.product_id WHERE u.username = ?";
+
+  if (search) {
+    query += " AND p.productname LIKE '%" + search + "%'";
+  }
+
+  console.log(query);
+
+  con.query(query, [req.username], function (err, result, fields) {
+    if (err) throw err;
+    res.json(result);
+  });
+
+};
+
+exports.getMerchantByPage = (req, res) => {
+  if (!req.isTokenValid) {
+    return res.json({ isLoggedIn: false, username: null });
+  }
+
+  const page    = req.params.page;
+  const limit   = req.params.limit;
+  const search  = req.params.search;
+  const filter  = req.params.filter;
+  const offset  = (page - 1) * limit;
+
+  var query = `SELECT p.productid AS MerchantID, up.quantity AS MerchantQuantity, p.productname AS MerchantName, p.imagepath AS MerchantImagePath
+               FROM users u
+               JOIN user_product up ON u.id = up.user_id
+               JOIN products p ON up.product_id = p.productid
+               WHERE u.username = ?`;
+
+  if (search) {
+    query += " AND p.productname LIKE '%" + search + "%'";
+  }
+
+  if (filter) {
+    if (filter == "Sort Asc") {
+      query += " ORDER BY p.productname ASC";
+    }
+    else if (filter == "Sort Desc") {
+      query += " ORDER BY p.productname DESC";
+    }
+  }
+
+  query += " LIMIT " + limit + " OFFSET " + offset;
+
+  con.query(query, [req.username], function (err, result, fields) {
+    if (err) throw err;
+    res.json(result);
+  });
+}
--- routes/merchantRoutes.js
+++ routes/merchantRoutes.js
@@ -5,6 +5,16 @@ const routes = express.Router();
 const { checkToken } = require("../middleware/authMiddleware");
 const merchantController = require("../controllers/merchantController");
 
+routes.get("/page/:page/limit/:limit/search/:search/filter/:filter", checkToken, merchantController.getMerchantByPage);
+routes.get("/page/:page/limit/:limit/search/:search", checkToken, merchantController.getMerchantByPage);
+routes.get("/page/:page/limit/:limit/filter/:filter", checkToken, merchantController.getMerchantByPage);
+routes.get("/page/:page/limit/:limit", checkToken, merchantController.getMerchantByPage);
+
 routes.get("/", checkToken, merchantController.getMerchants);
+
+routes.get("/count", checkToken, merchantController.getTotalMerchants);
+routes.get("/count/search/:search", checkToken, merchantController.getTotalMerchants);
+routes.get("/count/filter/:filter", checkToken, merchantController.getTotalMerchants);
+routes.get("/count/search/:search/filter/:filter", checkToken, merchantController.getTotalMerchants);
 
 module.exports = routes;
